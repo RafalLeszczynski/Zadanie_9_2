dcffffc15a556da40e29f96d73217427e696ab8a
sim1 <rafal.leszczynski@simplica.pl>
2017-09-12 11:04:07 +0200

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+// scripts.js
+
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+console.log(femaleNames);
+console.log(maleNames);
+
+var allNames = femaleNames.concat(maleNames);
+
+console.log(allNames);
+
+var newName = 'Marian';
+
+if(allNames.indexOf(newName)===-1){
+    allNames.push(newName);
+}
+
+console.log(allNames);
+
+var newName = 'Asia';
+
+if(allNames.indexOf(newName)===-1){
+    allNames.push(newName);
+}
+
+console.log(allNames);
